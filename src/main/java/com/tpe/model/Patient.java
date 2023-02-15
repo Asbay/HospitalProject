6f4fd106cdff4b63da68391b2ece9853e9dd8bfd
AsBay <asenabayhan@outlook.de>
2023-02-15 12:58:25 +0100

    2

--- src/main/java/com/tpe/model/Patient.java
+++ src/main/java/com/tpe/model/Patient.java
@@ -2,30 +2,32 @@ package com.tpe.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name="tbl_patient")
 public class Patient {
     @Id
     @Setter(AccessLevel.NONE)
     private Long id;
 
-
+    @NotNull(message= "Please provide your name")
+    @Column(length = 50, nullable = false)
     private String name;
-
+    @NotNull(message= "Please provide your gender MALE or FEMALE")
+    @Column(length =10, nullable = false)
     private Gender gender;
     private Integer dateOfBirth;
 
     private City city;
-
+    @NotNull(message= "Please provide your address")
+    @Column(length = 50, nullable = false)
     private String  address;
 
     private boolean healthInsurance;
